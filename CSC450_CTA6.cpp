5377db919f2155419a8f0355ea87d71b4538fa48
jakepalmer <jpalmer515@gmail.com>
2025-02-01 10:47:15 -0700

    added mutex locks and a break between functions

--- CSC450_CTA6.cpp
+++ CSC450_CTA6.cpp
@@ -3,22 +3,32 @@
 
 using namespace std;
 
+//mutual exclusion lock
+mutex m;
+
 void countingUp() {
+
+    //locking the resources used for this thread
+    m.lock();
     int counter = 0;
 
     for (int i = 0; i < 21; i++) {
         std::cout << counter << "\n";
         counter++;
     }
+    std::cout << "-BREAK- \n";
+    m.unlock();
 }
 
 void countingDown() {
+    m.lock();
     int counter = 20;
 
-    for (int i = 20; i > 0; i--) {
+    for (int i = 20; i >= 0; i--) {
         std::cout << counter << "\n";
         counter--;
     }
+    m.unlock();
 }
 
 int main() {
