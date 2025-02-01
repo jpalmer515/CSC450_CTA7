84ac0f38cbe72f247210b3181bbae56b526bfc97
jakepalmer <jpalmer515@gmail.com>
2025-02-01 10:40:32 -0700

    creating a countingDown function. added a newThread call in main, for countingDown. threads are running concurrently, trying to synchronize.

--- CSC450_CTA6.cpp
+++ CSC450_CTA6.cpp
@@ -12,10 +12,26 @@ void countingUp() {
     }
 }
 
-void countingDown() {}
+void countingDown() {
+    int counter = 20;
+
+    for (int i = 20; i > 0; i--) {
+        std::cout << counter << "\n";
+        counter--;
+    }
+}
 
 int main() {
 
+    //calling a new thread function to count up
     thread newThread(countingUp);
 
+    //calling a new thread function to count down
+    thread newThread2(countingDown);
+
+    //synchronizing threads
+    newThread.join();
+    newThread2.join();
+
+    return 0;
 }
